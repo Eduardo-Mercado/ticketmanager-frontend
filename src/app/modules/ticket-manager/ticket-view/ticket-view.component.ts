4df1741a08f21bc1549145c78539cdd64eb508cc
Eduardo Mercado <mercadohedward@gmail.com>
2020-03-27 17:54:02 -0600

    no message

--- src/app/modules/ticket-manager/ticket-view/ticket-view.component.ts
+++ src/app/modules/ticket-manager/ticket-view/ticket-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TicketAgent, TicketFile } from 'src/app/core/models/ticket-manager/ticket.model';
 import { TicketService } from 'src/app/core/services/task-manager/ticket.service';
 import { IconFile } from 'src/app/core/helpers/IconFile';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
   selector: 'app-ticket-view',
@@ -15,9 +16,57 @@ export class TicketViewComponent implements OnInit {
   ticketSelected: TicketAgent;
   isready = false;
   showDetail: boolean;
+  nvista: number;
+
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+      spellcheck: true,
+      height: 'auto',
+      minHeight: '16em',
+      maxHeight: 'auto',
+      width: 'auto',
+      minWidth: '0',
+      translate: 'yes',
+      enableToolbar: true,
+      showToolbar: true,
+      placeholder: 'Enter text here...',
+      defaultParagraphSeparator: '',
+      defaultFontName: '',
+      defaultFontSize: '',
+      fonts: [
+        {class: 'arial', name: 'Arial'},
+        {class: 'times-new-roman', name: 'Times New Roman'},
+        {class: 'calibri', name: 'Calibri'},
+        {class: 'comic-sans-ms', name: 'Comic Sans MS'}
+      ],
+      customClasses: [
+      {
+        name: 'quote',
+        class: 'quote',
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: 'titleText',
+        class: 'titleText',
+        tag: 'h1',
+      },
+    ],
+    uploadUrl: 'v1/image',
+    uploadWithCredentials: false,
+    sanitize: true,
+    toolbarPosition: 'top',
+    toolbarHiddenButtons: [
+      ['bold', 'italic'],
+      ['fontSize']
+    ]
+};
   constructor(private service: TicketService) { }
 
   ngOnInit() {
+    this.nvista = 0;
     this.service.getTicketsByAgent(0).subscribe((data: TicketAgent[]) => {
       this.listTickets = data;
       this.listTickets.forEach(element => {
@@ -64,5 +113,13 @@ export class TicketViewComponent implements OnInit {
       });
     });
     this.showDetail = true;
+  }
+
+  resolution() {
+    this.nvista = 1;
+
+  }
+  onBack() {
+    this.nvista = 0;
   }
 }
